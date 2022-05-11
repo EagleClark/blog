1b8e0b341327a9dc3c25e5e17bfe89030b918a84
Eagle Ye <eagleclark@163.com>
2022-05-12 00:02:07 +0800

    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
+    'plugin:react/jsx-runtime',
     'plugin:react/recommended',
-    'standard'
+    'standard',
+    'eslint:recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
   },
   plugins: [
     'react',
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
   rules: {
-  }
-}
+    'space-before-function-paren': ['error', {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'always',
+    }],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.less'
+import './App.less';
 
 function App() {
-
   return (
     <div className="App">
       <div className="test">
 
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
