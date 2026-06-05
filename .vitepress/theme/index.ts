a12246c56aba619489ad062f1c718c6c5ed5f3f0
EagleClark <eaglelljl@163.com>
2026-06-06 00:41:36 +0800

    feat: 新增giscus

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,11 @@
+import { h } from "vue";
 import { type Theme, inBrowser } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import './styles.css'
 import confetti from './components/confetti.vue';
 import busuanzi from 'busuanzi.pure.js';
 import VisitorPanel from './components/VisitorPanel.vue';
+import Layout from './components/Layout.vue';
 
 export default {
   extends: DefaultTheme,
@@ -18,4 +20,9 @@ export default {
       };
     }
   },
+  Layout: () => {
+    return h(Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    });
+  }
 } satisfies Theme;
